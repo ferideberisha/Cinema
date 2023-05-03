4354aedca9ce097e179e44c35c782837ee3ccd96
ferideberisha <feride.berisha2@student.uni-pr.edu>
2023-05-03 12:11:06 +0200

    Added admin route and model
    Removed isAdmin field from user

--- backend/index.js
+++ backend/index.js
@@ -2,13 +2,15 @@ const express = require("express");
 const mongoose = require("mongoose");
 const cors = require("cors");
 const dotenv = require("dotenv").config();
-const usersRoute = require("./routes/usersRoute");
 const path = require("path");
+const usersRoute = require("./routes/usersRoute");
+const staffRoute = require("./routes/staffRoute");
 
 const app = express();
 
 app.use(express.json());
 app.use("/api/users", usersRoute);
+app.use("/api/staff", staffRoute);
 
 app.use(cors());
 
