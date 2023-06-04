7f79fb7f38578b364fe674ab170886b66511e725
ferideberisha <93921511+ferideberisha@users.noreply.github.com>
2023-06-04 11:27:26 +0200

    Modified routes, added a new model and made some changes in api folder

--- backend/routes/staff/staffRoute.js
+++ backend/routes/staff/staffRoute.js
@@ -14,19 +14,26 @@ router.get("/:id", staffController.staff_get);
 router.put("/:id", staffController.staff_update);
 router.delete("/:id", staffController.staff_delete);
 
-// movie routes
-router.post("/movies-url", staffController.movie_post);
-router.delete("/movies-url/:id", staffController.movie_delete);
-router.get("/movies-url/all", staffController.movie_list);
-router.get("/movies-url/:id", staffController.movie_get);
-router.put("/movies-url/:id", staffController.movie_update);
-
 // theater routes
 router.post("/theater", staffController.theater_post);
 router.put("/theater/:id", staffController.theater_update);
 router.delete("/theater/:id", staffController.theater_delete);
 router.get("/theater/all", staffController.theater_list);
 router.get("/theater/:id", staffController.theater_get);
+
+// event routes
+router.post("/events", staffController.events_post);
+router.put("/events/:id", staffController.events_update);
+router.delete("/events/:id", staffController.events_delete);
+router.get("/events/all", staffController.events_list);
+router.get("/events/:id", staffController.events_get);
+
+// movie routes
+router.post("/movies-url", staffController.filma_post);
+router.delete("/movies-url/:id", staffController.filma_delete);
+router.get("/movies-url/all", staffController.filma_list);
+router.get("/movies-url/:id", staffController.filma_get);
+router.put("/movies-url/:id", staffController.filma_update);
 
 // use routes
 router.use("/contactus", require("./contactus/contactusRoute.js"));
